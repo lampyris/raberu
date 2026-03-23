658364d692ed1cbffc087188124bf1daf745865a
Lampyris <p.britanov@authentica.ru>
2026-03-23 14:45:31 +0400

    init

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,23 +13,39 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $users = [];
-        for ($i = 1; $i <= 3; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
             $user = new User();
             $user->setName("User $i");
             $manager->persist($user);
             $users[] = $user;
 
-            $ordersCount = rand(2, 4);
+            if ($i <= 3) {
+                $ordersCount = rand(5, 10);
+            } elseif ($i <= 7) {
+                $ordersCount = rand(2, 5);
+            } else {
+                $ordersCount = rand(0, 2);
+            }
+
             for ($j = 0; $j < $ordersCount; $j++) {
                 $order = new Order();
                 $order->setUser($user);
-                $order->setAmount(rand(1000, 5000));
+
+                if ($i <= 3) {
+                    $amount = rand(5000, 15000);
+                } elseif ($i <= 7) {
+                    $amount = rand(1000, 5000);
+                } else {
+                    $amount = rand(100, 1000);
+                }
+
+                $order->setAmount($amount);
                 $order->setCreatedAt(new \DateTimeImmutable());
                 $manager->persist($order);
             }
         }
 
-        for ($i = 1; $i <= 2; $i++) {
+        for ($i = 1; $i <= 3; $i++) {
             $vacancy = new Vacancy();
             $vacancy->setTitle("Active Vacancy $i");
             $vacancy->setIsActive(true);
@@ -43,7 +59,7 @@ class AppFixtures extends Fixture
 
         $manager->flush();
 
-        echo "Created " . count($users) . " users with orders\n";
-        echo "Created 3 vacancies (2 active, 1 inactive)\n";
+        echo "Created " . count($users) . " users with different order amounts\n";
+        echo "Created 4 vacancies (3 active, 1 inactive)\n";
     }
 }
